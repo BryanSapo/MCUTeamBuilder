71278257edbd8e54bfb95689b1ba93dd3b34cd1b
Bryan <bryan54045@gmail.com>
2024-01-04 17:07:45 +0800

    update
    this is an update of some css and some typo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html',stat='')
 @app.post("/group")
 def response():
     subjectCode=request.form.get('subjectCode')
@@ -12,6 +12,8 @@ def response():
     groupSize=int(request.form.get('groupSize'))
     print(subjectCode,classCode,groupSize)
     final_result=grouplib.webSearch(classCode,subjectCode,groupSize)
+    if len(final_result)<1:
+        return render_template('index.html',stat="找不到對應課程資訊")
     grouplib.logExcel(final_result)
     return send_file('./result.xlsx')
 
